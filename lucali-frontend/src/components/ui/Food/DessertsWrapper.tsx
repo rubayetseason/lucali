1692559c8d3c6c42988af1a30844224e3b040413
rubayet-islam <season.ss2001@gmail.com>
2023-10-17 20:14:07 +0600

    desserts page created with wrapper child

---
+++ lucali-frontend/src/components/ui/Food/DessertsWrapper.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import Desserts from "./Desserts";
+
+
+const DessetsWrapper = () => {
+  return (
+    <div>
+      <div className="py-10 md:py-7 bg-[#0F1D22]">
+        <Desserts></Desserts>
+      </div>
+    </div>
+  );
+};
+
+export default DessetsWrapper;
